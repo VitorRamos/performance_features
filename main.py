de7b8c0fe85fac9afafc9849d6d7103b855713ab
= <=>
2018-11-23 17:06:17 +0100

    measuring energy

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ def list_all_events():
 def from_pid(events):
     perf= profiler(events_groups=events)
     perf.start_counters(os.getpid())
-    for i in range(100):
-        print(perf.read_events())
+    data= []
+    for i in range(10):
+        data.append(perf.read_events())
         time.sleep(0.1)
+    df= pd.DataFrame(data, columns=[item for sublist in events for item in sublist])
+    with open('/sys/bus/event_source/devices/power/events/energy-pkg.scale') as f:
+        scale= float(f.read())
+    df['SYSTEMWIDE:RAPL_ENERGY_CORES']*=scale
+    df['SYSTEMWIDE:RAPL_ENERGY_PKG']*=scale
+    print(df)
 
 def workload(events):
     perf= profiler(program_args= ['./hello'], events_groups=events)
@@ -33,7 +40,6 @@ events= [['PERF_COUNT_HW_INSTRUCTIONS'],
 try:
     evs= list_events().get_supported_events()
     rapl_evs= [ e for e in evs if 'RAPL' in e ]
-
     rapl_evs= [['SYSTEMWIDE:'+e] for e in rapl_evs]
     rapl_evs= [['PERF_COUNT_HW_INSTRUCTIONS']]+rapl_evs
     print(rapl_evs)
