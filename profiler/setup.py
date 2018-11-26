6e43ba57e3e24091b834d6fe1972aff0e3353f42
= <=>
2018-11-26 15:10:30 +0100

    install modules

--- profiler/setup.py
+++ profiler/setup.py
@@ -1,15 +1,31 @@
-from distutils.core import setup, Extension
-from distutils.command.install_data import install_data
+import setuptools
+from distutils.core import  Extension
 
-setup(name='profiler',
-      version='0.1',
-      author='Vitor Ramos',
-      author_email='ramos.vitor89@gmail.com',
-      description='perf event wrapper for python',
-      packages=['profiler'],
-      package_dir={ 'profiler' : 'src' },
-      py_modules=['profiler.profiler'],
-      ext_modules=[Extension('profiler._workload',
-                  sources = ['src/workload.i', 'src/workload.cpp'],
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='profiler',
+    version='0.1',
+    packages=['perfmon', 'profiler'],
+    package_dir={ 'perfmon' : 'perfmon', 'profiler': 'profiler' },
+    py_modules=['perfmon.perfmon_int', 'profiler.profiler'],
+    ext_modules=[Extension('perfmon._perfmon_int',
+                sources = ['perfmon/perfmon_int.i'],
+                libraries = ['pfm'],
+                swig_opts=['-Iperfmon/include/']),
+                Extension('profiler._workload',
+                  sources = ['profiler/workload.i', 'profiler/workload.cpp'],
                   libraries = ['pfm'],
-                  swig_opts=['-c++'])])
+                  swig_opts=['-c++'])],
+    author="Vitor Ramos",
+    author_email="ramos.vitor89@gmail.com",
+    description="perf event wrapper for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VitorRamos/performance_features",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+    ],
+)
