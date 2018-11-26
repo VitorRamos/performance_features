799e81e813958c738f86a0e1edbd9426241d6ea9
= <=>
2018-11-26 09:25:36 +0100

    examples

--- main.py
+++ main.py
@@ -7,45 +7,43 @@ def list_all_events():
     print(list_events().get_supported_pmus())
     print(list_events().get_supported_events())
 
-def from_pid(events):
+def from_pid(events, pid):
     perf= profiler(events_groups=events)
-    perf.start_counters(os.getpid())
+    perf.start_counters(pid)
     data= []
-    for i in range(10):
+    while os.path.isdir('/proc/{}'.format(pid)):
         data.append(perf.read_events())
         time.sleep(0.1)
-    df= pd.DataFrame(data, columns=[item for sublist in events for item in sublist])
-    with open('/sys/bus/event_source/devices/power/events/energy-pkg.scale') as f:
-        scale= float(f.read())
-    df['SYSTEMWIDE:RAPL_ENERGY_CORES']*=scale
-    df['SYSTEMWIDE:RAPL_ENERGY_PKG']*=scale
-    print(df)
+    return data
 
 def workload(events):
     perf= profiler(program_args= ['./hello'], events_groups=events)
-    data= perf.run(sample_period= 0.01, reset_on_sample=False)
-    print(data)
+    data= perf.run(sample_period= 0.01, reset_on_sample=True)
+    return data
 
 def python_workload(events):
     perf= profiler(program_args= ['./hello'], events_groups=events)
-    data= perf.run_python(sample_period= 0.01, reset_on_sample=False)
-    print(data)
+    data= perf.run_python(sample_period= 0.01, reset_on_sample=True)
+    return data
 
 events= [['PERF_COUNT_HW_INSTRUCTIONS'], 
             ['PERF_COUNT_HW_CACHE_LL'], 
             ['PERF_COUNT_HW_BRANCH_INSTRUCTIONS'],
             ['PERF_COUNT_HW_BRANCH_MISSES'],
             ['PERF_COUNT_SW_PAGE_FAULTS']]
+evs= list_events().get_supported_events()
+rapl_evs= [ e for e in evs if 'RAPL' in e ]
+rapl_evs= [['SYSTEMWIDE:'+e] for e in rapl_evs]
+rapl_evs= [['PERF_COUNT_HW_INSTRUCTIONS']]+rapl_evs
 
 try:
-    evs= list_events().get_supported_events()
-    rapl_evs= [ e for e in evs if 'RAPL' in e ]
-    rapl_evs= [['SYSTEMWIDE:'+e] for e in rapl_evs]
-    rapl_evs= [['PERF_COUNT_HW_INSTRUCTIONS']]+rapl_evs
-    print(rapl_evs)
+    # print(rapl_evs)
     # list_all_events()
-    # workload(events)
-    # python_workload(events)
-    from_pid(rapl_evs)
+
+    data= workload(events)
+    print("C++ ", len(data), np.sum(data,axis=0))
+    # data= python_workload(events)
+    # print("Python ", len(data), np.sum(data,axis=0))
+    # data= from_pid(os.getppid(), rapl_evs)
 except RuntimeError as e:
     print(e.args[0])
