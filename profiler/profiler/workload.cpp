33da31cada4c9ea5d588735bdbc576bb06389678
= <=>
2018-12-11 12:03:44 +0100

    profile with no sampling

--- profiler/profiler/workload.cpp
+++ profiler/profiler/workload.cpp
@@ -88,6 +88,7 @@ vector<vector<signed long int>> Workload::run(double sample_perid, bool reset)
     ssize_t bytes_read;
     unsigned int i;
     int status;
+    int hangs= sample_perid>0?WNOHANG:0;
     samples.reserve(1000);
 
     if(!this->isAlive)
@@ -102,7 +103,7 @@ vector<vector<signed long int>> Workload::run(double sample_perid, bool reset)
     ptrace(PTRACE_CONT, pid, 0, 0);
     while(1)
     {
-        waitpid(pid, &status, WNOHANG);
+        waitpid(pid, &status, hangs);
         if (WIFEXITED(status))
             break;
         
