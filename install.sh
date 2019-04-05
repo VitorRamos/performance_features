777a667b7287bd6504f19cfcd10e98215484e79d
vitorramos <ramos.vitor89@gmail.com>
2019-04-05 14:59:48 +0200

    ok now...

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt install g++ gcc swig libpfm4-dev python3-dev
 cd profiler/
-pip install -r requirements.txt
 python setup.py build
 python setup.py install
+cd ..
+pip install -r requirements.txt
