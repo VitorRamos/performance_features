9ab66e9144832286a07a69ef2bf680aa7adf6ee9
vitorramos <ramos.vitor89@gmail.com>
2019-04-05 15:13:28 +0200

    sudo pls

--- kernel_parameters.sh
+++ kernel_parameters.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # disable paranoid (all users can read perf events)
-sysctl -w kernel.perf_event_paranoid=-1
+sudo sysctl -w kernel.perf_event_paranoid=-1
 # disable nmi watchdog (more PMC's left)
-sysctl -w kernel.nmi_watchdog=0
+sudo sysctl -w kernel.nmi_watchdog=0
 
 # change frequency permition
 # sudo chmod -R 777 /sys/devices/system/cpu/cpu*/cpufreq/
