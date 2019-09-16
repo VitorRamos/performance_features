ff3670e7b3a1d1b6d437e229237f168befd66ec4
vitorramos <ramos.vitor89@gmail.com>
2019-09-16 11:26:05 +0200

    specify python3

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install g++ gcc swig libpfm4-dev python3-dev
+apt update
+apt install g++ gcc swig libpfm4-dev python3-dev python3-pip
 cd profiler/
-python setup.py build
-python setup.py install
+python3 setup.py build
+python3 setup.py install
 cd ..
-pip install -r requirements.txt
+pip3 install -r requirements.txt
