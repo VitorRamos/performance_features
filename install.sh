4a13503ec34823ae01ebd5339fa32244bc48702e
vitorramos <ramos.vitor89@gmail.com>
2023-01-29 09:45:36 +0100

    update workflow

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-apt update
-apt install g++ gcc swig libpfm4-dev python3-dev python3-pip
+sudo apt update
+sudo apt install g++ gcc swig libpfm4-dev python3-dev python3-pip
 cd profiler/
 python3 setup.py build
 python3 setup.py install
-cd ..
-pip3 install -r requirements.txt
+
