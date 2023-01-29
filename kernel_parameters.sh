e38a6f236f785dc64406530a289a8008383b954b
vitorramos <ramos.vitor89@gmail.com>
2023-01-29 09:08:54 +0100

    remove frequency parameters

--- kernel_parameters.sh
+++ kernel_parameters.sh
@@ -4,8 +4,3 @@
 sudo sysctl -w kernel.perf_event_paranoid=-1
 # disable nmi watchdog (more PMC's left)
 sudo sysctl -w kernel.nmi_watchdog=0
-
-# change frequency permition
-# sudo chmod -R 777 /sys/devices/system/cpu/cpu*/cpufreq/
-# sudo chmod -R 777 /sys/devices/system/cpu/cpu*/online
-# sudo modprobe msr
